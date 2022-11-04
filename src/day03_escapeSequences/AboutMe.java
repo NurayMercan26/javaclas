f0f6f91c6a044c508a67f4e5bb8954d2d40cf5e9
NurayMercan26 <nuraymercan26@gmail.com>
2022-11-04 12:09:32 +0300

    Merge remote-tracking branch 'origin/javaclassss' into javaclassss second

--- src/day03_escapeSequences/AboutMe.java
+++ src/day03_escapeSequences/AboutMe.java
@@ -4,7 +4,7 @@ public class AboutMe {
     public static void main(String[] args) {
 
         System.out.println("My favorite music is \"Rock\"musics,\t\n\t\tMy favorite book is \"Data Structure in Java\" ");
-
+  //
 
 
 
